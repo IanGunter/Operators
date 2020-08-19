15278aabed27aa03fd27d6b2af217124c60ece51
Ian Gunter <iangunterofamerica@yahoo.com>
2020-08-19 08:47:05 -0600

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,27 @@
+﻿'Ian Gunter
+'RCET0265
+'Fall 2020
+'Operators
+'Repo
+
+Module Module1
+
+    Sub Main()
+        'let's do math!!
+        Console.WriteLine(2 + 2)
+        Console.ReadLine()
+        Console.WriteLine(7 - 1)
+        Console.ReadLine()
+        Console.WriteLine(10 / 2)
+        Console.ReadLine()
+        Console.WriteLine(5 * 5)
+        Console.ReadLine()
+        Console.WriteLine(7 \ 3)
+        Console.ReadLine()
+        Console.WriteLine((7 Mod 3) / 3)
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
